f2d6739b52c99ee3f099d2c108cb1b2ec96bf7a6
Tuan Nguyen <amtuannguyen@gmail.com>
2014-11-26 16:14:15 -0500

    working items/show.php

--- items/show.php
+++ items/show.php
@@ -1,20 +1,30 @@
 <?php 
-    $exhibit = get_current_record('exhibit', false); 
+    $exhibit = get_current_record('exhibit', false);
+    $sections = get_exhibit_sections($exhibit);
 ?>
 
 <?php echo head(array('bodyclass' => 'exhibits', 'exhibit' => $exhibit)); ?>
 
-<h1 class="item_title"><?php echo item('Dublin Core', 'Title'); ?></h1>
-<h2><?= html_escape($exhibit->title)  ?>:  <span><a href="<?php echo exhibit_builder_exhibit_uri($exhibit, $section); ?>"><?= html_escape($section->title) ?></a></span></h2>
-<h3><?php echo item('Dublin Core', 'Description'); ?></h3>
+<h1 class="item_title"><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+
+<h2><?= html_escape($exhibit->title)  ?></h2>
+
+<h3><?php echo metadata('item', array('Dublin Core', 'Description')); ?></h3>
     
 <div class="spacer"></div>
       
 <div class="left_side">
 			
-    <div id="itemfiles">
-    	<?php echo display_files_for_item(); ?>
+    <!-- The following returns all of the files associated with an item. -->
+    <?php if (metadata('item', 'has files')): ?>
+    <div id="itemfiles" class="element">
+        <?php if (get_theme_option('Item FileGallery') == 1): ?>
+        <div class="element-text"><?php echo item_image_gallery(); ?></div>
+        <?php else: ?>
+        <div class="element-text"><?php echo files_for_item(); ?></div>
+        <?php endif; ?>
     </div>
+    <?php endif; ?>
             
     <div class="spacer"></div>    
 
@@ -27,29 +37,29 @@
     <div class="jq_simple_tab_container">
         <div id="tab1" class="jq_simple_tab_content">
             <h3>Description</h3>
-            <?php echo item('Dublin Core', 'Description'); ?>
+            <?php echo metadata('item', array('Dublin Core', 'Description')); ?>
         
             <h3>Subject</h3>
-            <?php echo item('Dublin Core', 'Subject'); ?>
+            <?php echo metadata('item', array('Dublin Core', 'Subject')); ?>
         
             <h3>Date</h3>
-            <?php echo item('Dublin Core', 'Date'); ?>
+            <?php echo metadata('item', array('Dublin Core', 'Date')); ?>
         
             <h3>All Titles</h3>
             <ul>
-                <?php foreach (item('Dublin Core', 'Title', 'all') as $title): ?>
+                <?php foreach (metadata('item', array('Dublin Core', 'Title'), array('all' => true)) as $title): ?>
                     <li class="item-title"><?php echo $title; ?></li>
                 <?php endforeach ?>
             </ul>
         </div>
 
         <div id="tab2" class="jq_simple_tab_content">
-            <?php echo show_item_metadata(); ?>
+            <?php echo all_element_texts('item'); ?>
         </div>
 
         <div id="tab3" class="jq_simple_tab_content">
             <h3>Citation</h3>
-            <div id="citation-value" class="field-value"><?php echo item_citation(); ?></div>
+            <div id="citation-value" class="field-value"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
         </div>
     </div>
 
@@ -65,18 +75,18 @@
     
     <div class="spacer"></div>
 
-</div> <!-- end left_side __>
+</div> <!-- end left_side -->
     
 <div class="right_side">
 
     <h3><span class="inner">Pages...</span></h3>
-    <div class="inner"><?php echo exhibit_builder_page_nav(); ?></div>	
+    <div class="inner"><?php //echo exhibit_builder_page_nav(); ?></div>	
 
-    <?php if(count($item->Tags)): ?>
-        <h3><span class="inner">Tags</span></h3>
-        <div class="tags inner">
-		   <?php echo item_tags_as_string(); ?>	
-		</div>
+    <?php if (metadata('item', 'has tags')): ?>
+    <h3><span class="inner">Tags</span></h3>
+    <div class="tags inner">
+	   <?php echo  tag_string('item'); ?>	
+	</div>
     <?php endif;?>
         
     <div class="spacer"></div>
@@ -85,7 +95,7 @@
     
     <div id="year_selector" class="round">Select Year <a href="#"></a></div>
     <div id="exhibit_years" class="round">
-		<?php foreach($exhibit->Sections as $section): ?>			                          
+		<?php foreach($sections as $section): ?>
             <?php if (is_numeric(trim($section->title))): ?>
                 <span class="round"><a href="<?php echo exhibit_builder_exhibit_uri($exhibit, $section); ?>"><?php echo html_escape($section->title); ?></a></span>		
 			<?php endif; ?>         
@@ -95,7 +105,7 @@
     
     <h3><span class="inner">Browse</span></h3>
     <ul id="exhibit_sections">
-        <?php foreach($exhibit->Sections as $section): ?>			
+        <?php foreach($sections as $section): ?>			
             <?php if (!is_numeric($section->title)): ?>
                 <li><a class="exhibit_section" href="<?php echo exhibit_builder_exhibit_uri($exhibit, $section); ?>"><?php echo html_escape($section->title); ?></a></li>	
             <?php endif; ?>         
@@ -107,5 +117,7 @@
 </div> <!-- end right_side -->
 
 <div class="clear"></div>
+
+<?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 <?php echo foot(); ?>
