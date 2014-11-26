554b947bf82b16cce67c7e3c96e2df6dbc7f5216
Tuan Nguyen <amtuannguyen@gmail.com>
2014-11-26 12:26:58 -0500

    removed clickheat

--- common/footer.php
+++ common/footer.php
@@ -11,14 +11,5 @@
 
 <script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script type="text/javascript">try {var pageTracker = _gat._getTracker("UA-7143283-2");pageTracker._trackPageview();} catch(err) {}</script> 
 
-
-
-<script type="text/javascript" src="http://www.library.yorku.ca/clickheat/js/clickheat.js"></script>
-<script type="text/javascript">
-clickHeatSite = 'omeka';
-clickHeatGroup = 'mariposa';
-clickHeatServer = 'http://www.library.yorku.ca/clickheat/click.php';
-initClickHeat();
-</script>    
 </body>
 </html>
